022940df846066fe888fce673bbce3d2bd20ced5
Kim Orumchian <Kim@Kims-MacBook-Pro.local>
2015-06-02 15:42:15 -0700

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,21 @@
 require 'erb'
 
-x = 42
+x = 123321
 template = ERB.new "The value of x is: <%= x %>"
+puts template.result(binding)
+
+y = "HUGE"
+template = ERB.new "The value of y is: <%= y %>"
+puts template.result(binding)
+
+z = 22.5*445.6123
+template = ERB.new "The value of z is: <%= z %>"
+puts template.result(binding)
+
+zz_hash = { "Fred" => 22, "Nancy" => 19, "Vinh" => 24, "Alberto" => 30, "Rina" => 25 }
+template = ERB.new "The value of zz_hash is: <%= zz_hash %>"
+puts template.result(binding)
+
+yy_array = ["vanilla", "strawberry", "peach"]
+template = ERB.new "The value of yy_array is: <%= yy_array %>"
 puts template.result(binding)
